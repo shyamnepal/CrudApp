f0f09c3c3d846ddadae4387e8617347517bbef27
shyamnepal <shyamnepal595@gmail.com>
2023-02-27 22:20:14 +0545

    add custom javascript for show and hide from

---
+++ CrudeApp/wwwroot/js/custom.js
@@ -0,0 +1,20 @@
+﻿
+
+
+
+
+
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    const form = document.getElementById('form');
+
+    if (form.style.display === 'none') {
+        // 👇️ this SHOWS the form
+        form.style.display = 'block';
+    } else {
+        // 👇️ this HIDES the form
+        form.style.display = 'none';
+    }
+});
